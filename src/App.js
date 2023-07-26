6da9d7defc9e0eb095e3ede90c48daef0b777bf1
Abhishek kumar <abhi.arya97161@gmail.com>
2023-07-26 17:45:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function App() {
+
+  const [num1,setNum1] = useState(0);
+  const [num2,setNum2] = useState(0);
+  const [num3,setNum3] = useState(0);
+  const [interest,setInterest] = useState(0);
+
+  const calcSum = (e) =>{
+    e.preventDefault();
+    if(num1==0 || num2==0 || num3==0){
+      alert("Please enter valid details");
+    }
+    else{
+      setInterest(parseInt(num1*num2*num3)/100)
+      // console.log(Add)
+    } 
+  }
+
+  const handleClick = (e) =>{
+    e.preventDefault();
+    window.location.reload();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'>
+            <div className='heading_text'>
+              <h1 className='heading_one'>Simple Calculator</h1>
+              <p className='heading_two'>Calculate Your Simple Interest Eaisly</p>
+            </div>
+
+            <div className='total_amount_card'>
+              <div className='card_text '>
+              <h3 className='total_amount_heading'>₹ {interest}</h3>
+              <p className='total_amount_para'>Total simple interest</p>
+            </div>
+          </div>
+      
+
+        <form onSubmit={calcSum}>
+            <div className='input_area'>
+                <div className='input_field'>
+                <TextField type="number" value={num1 || ""} onChange={(e) => setNum1(e.target.value)} id="outlined-basic" label="₹ Principal amount" variant="outlined" />
+                </div>
+
+                <div className='input_field'>
+                <TextField type="number" value={num2 || ""} onChange={(e) => setNum2(e.target.value)} id="outlined-basic" label="Rate of interest (p.a) %" variant="outlined" />
+                </div>
+                
+                <div className='input_field'>
+                <TextField type="number" value={num3 || ""} onChange={(e) => setNum3(e.target.value)} id="outlined-basic" label="Time period ( Yr )" variant="outlined" />
+                </div>
+            </div>
+            <div className='button_collection'>
+              <Stack spacing={2} direction="row">       
+              <Button type='submit' className='btn_one' style={{backgroundColor: 'black'}} variant="contained">Calculate</Button>
+              <Button className='btn_one'  onClick={handleClick}  variant="outlined">Reset</Button>
+              </Stack>
+            </div>
+        </form>
+      </div>
     </div>
   );
 }
